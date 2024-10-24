36cb452f88786b9801f12bb71971f09ddf8feeca
Tomoaki Yagishita <tyagishi@gmail.com>
2024-10-24 18:14:41 +0900

    F - use DataStructure4.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/tyagishi/SDSDataStructure", from: "3.6.2"),
+        .package(url: "https://github.com/tyagishi/SDSDataStructure", from: "4.0.0"),
         .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.56.1"),
     ],
     targets: [
