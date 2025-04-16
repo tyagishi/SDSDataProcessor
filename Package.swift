09ff2ddd30ae306157c5644a00f32e73eb319480
tyagishi <tyagishi@gmail.com>
2025-04-16 21:37:10 +0900

    F - CustomRegex under WIP

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tyagishi/SDSDataStructure", from: "4.0.0"),
         .package(url: "https://github.com/tyagishi/SDSSwiftExtension", from: "2.1.4"),
+        .package(url: "https://github.com/tyagishi/SDSStringExtension", from: "1.4.0"),
         .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.56.1"),
     ],
     targets: [
